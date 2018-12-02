ecdb8b9fe88215827b1342709118adb587d43d79
Hem Shrestha <hereshem@gmail.com>
2018-12-02 17:36:40 +0545

    polls app templating done

--- polls/urls.py
+++ polls/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from polls.views import hello_world, question_list, question_detail, question_vote
 
+app_name = "polls"
+
 urlpatterns = [
-    path('', question_list),
-    path('<int:id>/', question_detail),
-    path('<int:id>/vote/', question_vote),
+    path('', question_list, name="list"),
+    path('<int:id>/', question_detail, name="detail"),
+    path('<int:id>/vote/', question_vote, name="vote"),
 ]
